1233ed67ef9f536a28201ba2dd210751fd5cd0dc
Denny McYuri <nordicwolf.d@gmail.com>
2022-02-01 14:39:54 +0200

    ****Add tab bar

--- Bankey/Supporting/AppDelegate.swift
+++ Bankey/Supporting/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginVC = LoginVC()
     let onboardingContainerVC = OnboardingContainerVC()
     let dummyVC = DummyViewController()
+    let mainVC = MainVC()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         
@@ -25,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginVC.delegate = self
         onboardingContainerVC.delegate = self
         dummyVC.logoutDeleagte = self
-        window?.rootViewController = loginVC
+        window?.rootViewController = mainVC
         
         return true
     }
